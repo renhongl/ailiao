d49701eec8707e21235215e6317bd6ace6cd48e1
renhongl <1075220132@qq.com>
2016-09-05 22:54:00 +0800

    Updated

---
+++ app/core/preLoad.js
@@ -0,0 +1,14 @@
+
+
+(function () {
+
+    let body = document.querySelector('body');
+    let loadingDiv = document.createElement('div');
+    let loadingImg = document.createElement('img');
+    loadingDiv.id = 'loadingDiv';
+    loadingImg.id = 'loadingImg';
+    loadingImg.src = 'images/loading.gif';
+    loadingDiv.appendChild(loadingImg);
+    body.appendChild(loadingDiv);
+    
+})();
